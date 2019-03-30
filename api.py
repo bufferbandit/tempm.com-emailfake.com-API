d323877394c641165947a95966a6de7b64edc267
kernelland <kernelland@protonmail.ch>
2019-03-30 09:15:09 +0100

    Update api.py

--- api.py
+++ api.py
@@ -1,60 +1,57 @@
+from urllib.request import build_opener, HTTPCookieProcessor, Request
 from xml.etree.ElementTree import ElementTree
 from lxml import etree,html
+from pprint import pprint
 
-import requests
 
-
-class TEMPM_API:
-
-	def __init__(self,email=None):
-		self.session = requests.session()
+def get_emails(email=None,only_last_email=False,):
+		
+		session = build_opener(HTTPCookieProcessor())
+		base_url="http://tempm.com"
+		
 		if email:
 			username,domain = email.split("@")
-			r = self.session.get("https://tempm.com/{domain}/{username}".format(
+			r = session.open( "{base_url}/{domain}/{username}".format(
+               base_url=base_url,
 			   username=username,	
 			   domain=domain
 				)
 			)
 		else:
-			r = self.session.get("https://tempm.com")
+			r = session.open(base_url)
 			
-		self.tree = etree.HTML(r.text)
-		self.email_urls = []
-		self.emails = []
-		self.email_addr = self.tree.xpath("//span[@id='email_ch_text']")[0].text
-
+		tree = etree.HTML(r.read().decode("utf-8"))
+		email_urls = []
+		emails = []
+		email_addr = tree.xpath("//span[@id='email_ch_text']")[0].text
 		
-
-	def get_emails(self):
 		try:
 			email_urls = []
-			collected_emails =  self.tree.xpath("//div[@id='email-table']")[0].getchildren()
+			collected_emails =  tree.xpath("//div[@id='email-table']")[0].getchildren()
 			for email_path_url in collected_emails:
-				                
+					
 				try:
-					email_url = "https://tempm.com" + email_path_url.attrib["href"]
+					email_url = base_url + "/" + email_path_url.attrib["href"]
 					_xpath = "//div[@id='iddelet2']"
 					_break = False
 				except KeyError:
-					email_url = "https://tempm.com"
+					email_url = base_url
 					_xpath = "//div[@class='e7m row list-group-item']"
 					_break = True
 					
 				email_urls.append(email_url)
 				
-				r = self.session.get(email_url)
-				tree = etree.HTML(r.text)
+				r = session.open(email_url)
+				tree2 = etree.HTML(r.read().decode("utf-8"))
 				
-				_html = tree.xpath(_xpath)[0]
+				_html = tree2.xpath(_xpath)[0]
 				
 				childs = _html.getchildren()
 				
 				meta_data = childs[0]
 				body = childs[3]
 				
-				
 				body_childs = meta_data.getchildren()
-				
 				
 				email_meta = {
                     
@@ -65,11 +62,16 @@ class TEMPM_API:
                     "body"       :  body.getchildren()[2][0].text
                             
                             }
-				self.emails.append(email_meta)
-				if _break:break
+				emails.append(email_meta)
+				if _break or only_last_email:break
 				else:continue
 
-			self.email_urls = email_urls	
-			return email_urls	
-		except IndexError: 
-			return []      
+			email_urls = email_urls	
+			email_urls = list(set(email_urls))
+			#emails = [i for n, i in enumerate(emails) if i not in emails[n + 1:]]
+		except IndexError:
+			pass
+    
+		finally:
+			return {"email_addr":email_addr,"inbox":emails}
+        
